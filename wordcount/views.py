c9d4140c016c1195ac116b09b6487a0fa79bddab
Anuradha Kalaskar <anuradha.kalaskar@gmail.com>
2018-08-31 14:24:43 +0530

    Created about file, actual couting operations and some page redirections

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,8 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 def home(request):
     return render(request, 'home.html')
 
+def about(request):
+    return render(request, 'about.html')
+
 def count(request):
-    return render(request, 'count.html')
+    fulltext = request.GET['fulltext']
+    wordList = fulltext.split()
+
+    wordDict = dict()
+    for word in wordList:
+        if word in wordDict:#Increase
+            wordDict[word] += 1
+        else:
+            wordDict[word] = 1
+
+    sortedWords = sorted(wordDict.items(), key=operator.itemgetter(1), reverse=True)
+    return render(request, 'count.html', {'fulltext': fulltext, 'count':len(wordList), 'sortedWords': sortedWords} )
